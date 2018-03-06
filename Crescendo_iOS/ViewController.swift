1de8a563d9ccc09d41d20880147faa02bd99bea4
Andrew Stears <astears@andrews-macbook-pro.local>
2018-03-06 15:15:43 -0800

    Initial Commit

---
+++ Crescendo_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crescendo_iOS
+//
+//  Created by Andrew Stears on 3/6/18.
+//  Copyright © 2018 Andrew Stears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
